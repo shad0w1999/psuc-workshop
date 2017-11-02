5911850e7227101c8f41dbd4ccda09d888eb5270
Hardik Singh <shardik99@gmail.com>
2017-11-03 01:31:24 +0530

    Added code for Introduction

---
+++ Day1/1Introduction/variable.cpp
@@ -0,0 +1,17 @@
+/* This program adds two integer values and displays the results */
+#include <iostream>
+using namespace std;
+int main (){
+
+    // Declare variables
+    int value1, value2, sum;
+
+    // Assign values and calculate their sum
+    value1 = 50;
+    value2 = 40;
+    sum = value1 + value2;
+    
+    // Display the result
+    cout<<"The sum of "<<value1<<" and "<<value2<<" is"<<sum;
+    return 0;
+}
